19fa9066a9f1e5aaddc26f6558bd69bc93c7c8d3
Tyler Hart <tyler@manitonetworks.com>
2016-06-18 22:04:47 -0400

    Moved list of second-level domains here

--- dns_base.py
+++ dns_base.py
@@ -9,3 +9,26 @@ def init():
 	dns_cache = {}
 	dns_cache["Records"] = {}
 	dns_cache["Prune"] = int(time.time() + 1800)
+
+	global second_level_domains
+	second_level_domains = {
+	"co.id",
+	"co.in",
+	"co.jp",
+	"co.nz",
+	"co.uk",
+	"co.za",
+	"com.ar",
+	"com.au",
+	"com.bn",
+	"com.br",
+	"com.cn",
+	"com.gh",
+	"com.hk",
+	"com.mx",
+	"com.sg",
+	"edu.au",
+	"net.au",
+	"net.il",
+	"org.au"
+	}
