6292843335b82ef6f737445ced973f87ae63b62e
Tyler Hart <Tyler Hart>
2016-09-20 21:37:13 -1000

    Began recording MAC OUIs

---
+++ mac_numbers.py
@@ -0,0 +1,30 @@
+# Copyright 2016, Manito Networks, LLC. All rights reserved
+#
+# Last modified 4/25/2016
+
+# NOTE: This is updated as we have time, and is by no means comprehensive yet.
+
+mac_prefix = {
+"00CDFE":"Apple",
+"18AF61":"Apple",
+
+"CC46D6":"Cisco",
+"58AC78":"Cisco",
+
+"0050BA":"D-Link",
+"00179A":"D-Link",
+"1CBDB9":"D-Link",
+"9094E4":"D-Link",
+"28107B":"D-Link",
+"1C7EE5":"D-Link",
+"C4A81D":"D-Link",
+
+"3CD92B":"Hewlett Packard",
+"9C8E99":"Hewlett Packard",
+"B499BA":"Hewlett Packard",
+"1CC1DE":"Hewlett Packard",
+
+"2C3033":"Netgear",
+
+"FFFFFF":"Broadcast"
+}
