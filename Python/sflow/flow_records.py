6d09abc16508d340ecd5103193f94290e179f875
Tyler Hart <Tyler Hart>
2016-11-20 22:22:52 -0500

    Huge sFlow updates all around

--- Python/sflow/flow_records.py
+++ Python/sflow/flow_records.py
@@ -20,6 +20,8 @@ def raw_packet_header(data):
 		mac_addresses = parse_eth_header(sample_data["Header"])
 		sample_data["Destination MAC"] = mac_addresses[0]
 		sample_data["Source MAC"] = mac_addresses[1]
+	
+	data.done()
 	return sample_data
 
 # Ethernet Frame Data (Flow, Enterprise 0, Format 2)
@@ -29,6 +31,7 @@ def eth_frame_data(data):
 	sample_data["Source MAC"] = data.unpack_string()
 	sample_data["Destination MAC"] = data.unpack_string()
 	sample_data["Type"] = data.unpack_uint()
+	data.done()
 	return sample_data
 
 # IPv4 Data (Flow, Enterprise 0, Format 3)
@@ -42,6 +45,7 @@ def ipv4_data(data):
 	sample_data["Destination Port"] = data.unpack_uint()
 	sample_data["TCP Flags"] = data.unpack_uint()
 	sample_data["Type of Service"] = data.unpack_uint()
+	data.done()
 	return sample_data
 
 # IPv6 Data (Flow, Enterprise 0, Format 4)
@@ -55,6 +59,7 @@ def ipv6_data(data):
 	sample_data["Destination Port"] = data.unpack_uint()
 	sample_data["TCP Flags"] = data.unpack_uint()
 	sample_data["IP Priority"] = data.unpack_uint()
+	data.done()
 	return sample_data
 
 # Extended Switch Data (Flow, Enterprise 0, Format 1001)
@@ -64,6 +69,7 @@ def extended_switch_data(data):
 	sample_data["Source Priority"] = data.unpack_uint()
 	sample_data["Destination VLAN"] = data.unpack_uint()
 	sample_data["Destination Priority"] = data.unpack_uint()
+	data.done()
 	return sample_data
 
 # Extended Router Data (Flow, Enterprise 0, Format 1002)
@@ -80,66 +86,121 @@ def extended_router_data(data):
 
 	sample_data["Source Mask Length"] = int(data.unpack_uint())
 	sample_data["Destination Mask Length"] = int(data.unpack_uint())
+	data.done()
 	return sample_data
 
 # Extended Gateway Data (Flow, Enterprise 0, Format 1003)
 def extended_gateway_data(data):
 	sample_data = {}
-	sample_data["header_protocol"] = int(data.unpack_uint())
+	data.done()
 	return sample_data
 
 # Extended User Data (Flow, Enterprise 0, Format 1004)
 def extended_user_data(data):
 	sample_data = {}
-	sample_data["header_protocol"] = int(data.unpack_uint())
+	data.done()
 	return sample_data
 
 # Extended URL Data (Flow, Enterprise 0, Format 1005)
 def extended_url_data(data):
 	sample_data = {}
-	sample_data["header_protocol"] = int(data.unpack_uint())
+	sample_data["Connection Direction"] = url_direction(int(data.unpack_uint()))
+	sample_data["URL"] = data.unpack_string()
+	sample_data["Host"] = data.unpack_string()
+	data.done()
 	return sample_data
 
 # Extended MPLS Data (Flow, Enterprise 0, Format 1006)
 def extended_mpls_data(data):
 	sample_data = {}
-	sample_data["header_protocol"] = int(data.unpack_uint())
+	data.done()
 	return sample_data
 
 # Extended NAT Data (Flow, Enterprise 0, Format 1007)
 def extended_nat_data(data):
 	sample_data = {}
-	sample_data["header_protocol"] = int(data.unpack_uint())
+	data.done()
 	return sample_data
 
 # Extended MPLS Tunnel (Flow, Enterprise 0, Format 1008)
 def extended_mpls_tunnel(data):
 	sample_data = {}
-	sample_data["header_protocol"] = int(data.unpack_uint())
+	sample_data["Tunnel LSP Name"] = data.unpack_string()
+	sample_data["Tunnel ID"] = int(data.unpack_uint())
+	sample_data["Tunnel COS"] = int(data.unpack_uint())
+	data.done()
 	return sample_data
 
 # Extended MPLS VC (Flow, Enterprise 0, Format 1009)
 def extended_mpls_vc(data):
 	sample_data = {}
-	sample_data["header_protocol"] = int(data.unpack_uint())
+	sample_data["VC Instance Name"] = data.unpack_string()
+	sample_data["VLL VC Instance ID"] = int(data.unpack_uint())
+	sample_data["VC Label COS"] = int(data.unpack_uint())
+	data.done()
 	return sample_data
 
 # Extended MPLS FEC (Flow, Enterprise 0, Format 1010)
 def exteded_mpls_fec(data):
 	sample_data = {}
-	sample_data["header_protocol"] = int(data.unpack_uint())
+	sample_data["MPLS FTN Description"] = data.unpack_string()
+	sample_data["MPLS FTN Mask"] = int(data.unpack_uint())
+	data.done()
 	return sample_data
 
 # Extended MPLS LVP FEC (Flow, Enterprise 0, Format 1011)
 def extended_mpls_lvp_fec(data):
 	sample_data = {}
-	sample_data["header_protocol"] = int(data.unpack_uint())
+	sample_data["MPLS FEC Address Prefix Length"] = int(data.unpack_uint())
+	data.done()
 	return sample_data
 
 # Extended VLAN Tunnel (Flow, Enterprise 0, Format 1012)
 def extended_vlan_tunnel(data):
 	sample_data = {}
-	sample_data["header_protocol"] = int(data.unpack_uint())
+	sample_data["VLAN Stack"] = int(data.unpack_uint())
+	data.done()
+	return sample_data
+
+# Extended 802.11 Payload (Flow, Enterprise 0, Format 1013)
+def extended_wlan_payload(data):
+	sample_data = {}
+	data.done()
+	return sample_data
+
+# Extended 802.11 RX (Flow, Enterprise 0, Format 1014)
+def extended_wlan_rx(data):
+	sample_data = {}
+	sample_data["SSID"] = data.unpack_fstring(32)
+	sample_data["BSSID"] = data.unpack_string()
+	sample_data["802.11 Version"] = wlan_version(int(data.unpack_uint()))
+	sample_data["802.11 Channel"] = int(data.unpack_uint())
+	sample_data["Speed"] = data.unpack_uhyper()
+	sample_data["RSNI"] = int(data.unpack_uint())
+	sample_data["RCPI"] = int(data.unpack_uint())
+	sample_data["Packet Duration"] = int(data.unpack_uint())
+	data.done()
+	return sample_data
+
+# Extended 802.11 TX (Flow, Enterprise 0, Format 1015)
+def extended_wlan_tx(data):
+	sample_data = {}
+	sample_data["SSID"] = data.unpack_fstring(32)
+	sample_data["BSSID"] = data.unpack_string()
+	sample_data["802.11 Version"] = wlan_version(int(data.unpack_uint()))
+	sample_data["Transmissions"] = wlan_transmissions(int(data.unpack_uint()))
+	sample_data["Packet Duration"] = int(data.unpack_uint())
+	sample_data["Retransmission Duration"] = int(data.unpack_uint())
+	sample_data["802.11 Channel"] = int(data.unpack_uint())
+	sample_data["Speed"] = data.unpack_uhyper()
+	sample_data["Power mW"] = int(data.unpack_uint())
+	data.done()
+	return sample_data
+
+# Extended 802.11 Aggregation (Flow, Enterprise 0, Format 1016)
+def extended_wlan_aggregation(data):
+	sample_data = {}
+	data.done()
 	return sample_data
 
 # IPv4 Socket (Flow, Enterprise 0, Format 2100)
@@ -153,6 +214,7 @@ def ipv4_socket(data):
 	sample_data["Source Port"] = int(data.unpack_uint())
 	sample_data["Destination Port"] = int(data.unpack_uint())
 	# Need to add category based on source / dest port - FIX
+	data.done()
 	return sample_data
 
 # IPv6 Socket (Flow, Enterprise 0, Format 2101)
@@ -166,4 +228,23 @@ def ipv6_socket(data):
 	sample_data["Source Port"] = int(data.unpack_uint())
 	sample_data["Destination Port"] = int(data.unpack_uint())
 	# Need to add category based on source / dest port - FIX
+	data.done()
 	return sample_data
+
+# Extended TCP Information (Flow, Enterprise 0, Format 2209)
+def extended_tcp_info(data):
+	datagram = {}
+	datagram["Packet Direction"] = packet_direction(data.unpack_uint()) # Parsed packet direction
+	datagram["Cached Effective MSS"] = packet_direction(data.unpack_uint())
+	datagram["Max Received Segment Size"] = packet_direction(data.unpack_uint())
+	datagram["Un-ACKed Packets"] = packet_direction(data.unpack_uint())
+	datagram["Lost Packets"] = packet_direction(data.unpack_uint())
+	datagram["Retransmitted Packets"] = packet_direction(data.unpack_uint())
+	datagram["PMTU"] = packet_direction(data.unpack_uint())
+	datagram["RTT ms"] = packet_direction(data.unpack_uint())
+	datagram["RTT Variance ms"] = packet_direction(data.unpack_uint())
+	datagram["Sending Congestion Window"] = packet_direction(data.unpack_uint())
+	datagram["Reordering"] = packet_direction(data.unpack_uint())
+	datagram["Minimum RTT ms"] = packet_direction(data.unpack_uint())
+	data.done()
+	return datagram
--- Python/sflow/sflow_samples.py
+++ Python/sflow/sflow_samples.py
@@ -8,7 +8,7 @@ from sflow_parsers import * # Functions to parse headers and format numbers
 # Flow Sample [0,1] 
 def flow_sample(unparsed_data):
 	sample_cache = {}
-	sample_cache["Sequence Number"] = unparsed_data.unpack_uint() # Sequence number
+	sample_cache["Sequence"] = unparsed_data.unpack_uint() # Sequence number
 	source_id_type_index = source_type_index_parser(unparsed_data.unpack_uint()) # Parse Source and Index types
 	sample_cache["Source Type"] = source_id_type_index[0] # Source type
 	sample_cache["Source Index"] = source_id_type_index[1] # Source index
@@ -23,7 +23,7 @@ def flow_sample(unparsed_data):
 # Counter Sample [0,2] 
 def counter_sample(unparsed_data):
 	sample_cache = {}
-	sample_cache["Sequence Number"] = unparsed_data.unpack_uint() # Sequence number
+	sample_cache["Sequence"] = unparsed_data.unpack_uint() # Sequence number
 	source_id_type_index = source_type_index_parser(unparsed_data.unpack_uint()) # Parse Source and Index types
 	sample_cache["Source ID Type"] = source_id_type_index[0] # Source ID Type
 	sample_cache["Source ID Index"] = source_id_type_index[1] # Source ID Index
@@ -33,7 +33,7 @@ def counter_sample(unparsed_data):
 # Expanded Flow Sample [0,3] 
 def expanded_flow_sample(unparsed_data):
 	sample_cache = {}
-	sample_cache["Sequence Number"] = unparsed_data.unpack_uint() # Sequence number
+	sample_cache["Sequence"] = unparsed_data.unpack_uint() # Sequence number
 	sample_cache["Source Type"] = unparsed_data.unpack_uint() # Source type
 	sample_cache["Source Index"] = unparsed_data.unpack_uint() # Source index
 	sample_cache["Sampling Rate"] = unparsed_data.unpack_uint() # Sample rate * n packets
@@ -49,13 +49,13 @@ def expanded_flow_sample(unparsed_data):
 # Expanded Flow Sample [0,4] 
 def expanded_counter_sample(unparsed_data):
 	sample_cache = {}
-	sample_cache["Sequence Number"] = unparsed_data.unpack_uint() # Sequence number
+	sample_cache["Sequence"] = unparsed_data.unpack_uint() # Sequence number
 	sample_cache["Source ID Type"] = unparsed_data.unpack_uint() # Source ID Type
 	sample_cache["Source ID Index"] = unparsed_data.unpack_uint() # Source ID Index
 	sample_cache["Record Count"] = unparsed_data.unpack_uint() # Counter Records (num)
 	return sample_cache
 
-# Func to consolidate some code
+# Pick Flow, Expanded Flow, Counter, Expanded Counter types to unpack
 def sample_picker(enterprise_format,unpacked_sample_data):
 	if enterprise_format == [0,1]:
 		return flow_sample(unpacked_sample_data) # Parse the sample header
