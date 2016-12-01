e6c628549b92c44a97ef7b5f2fa1e9791a141fd3
Tyler Hart <Tyler Hart>
2016-12-01 01:25:44 -0500

    Handle Python Pip issues with Curator

--- Install/ubuntu_install_elk5.sh
+++ Install/ubuntu_install_elk5.sh
@@ -31,9 +31,10 @@ apt-get -y install elasticsearch kibana
 # Resolving Python dependencies
 echo "Install Python dependencies"
 apt-get install python-pip -y
-pip install --upgrade pip
 pip install --upgrade setuptools
+pip install --upgrade pip
 pip install -r $flow_analyzer_dir/requirements.txt
+pip install --upgrade elasticsearch-curator
 
 # Set the Elasticsearch cluster details
 echo "Set the Elasticsearch cluster details"
