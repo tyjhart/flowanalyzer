28ef0d1b74178f7985b56dcc80f0270208505701
Tyler Hart <Tyler Hart>
2016-12-01 01:01:46 -0500

    Python pip upgrade setuptools for ES5

--- Install/ubuntu_install_elk5.sh
+++ Install/ubuntu_install_elk5.sh
@@ -32,6 +32,7 @@ apt-get -y install elasticsearch kibana
 echo "Install Python dependencies"
 apt-get install python-pip -y
 pip install --upgrade pip
+pip install --upgrade setuptools
 pip install -r $flow_analyzer_dir/requirements.txt
 
 # Set the Elasticsearch cluster details
